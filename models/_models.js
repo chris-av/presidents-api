bc22e12818bfd43c3d7fa5e57d2d4ac3d6d40710
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-23 00:00:56 -0700

    convert to typescript, move into src dir

--- models/_models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const President = require('./President');
-const CabinetMember = require('./CabinetMember');
-
-module.exports = {
-  President: mongoose.model('president', President),
-  CabinetMember: mongoose.model('cabinet-member', CabinetMember),
-}
--- util/delay.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(seconds) {
-  return new Promise((resolve, reject) => {
-    console.log(`waiting ${seconds} seconds ... \n`);
-    setTimeout(() => {
-      resolve('');
-    }, seconds * 1000)
-  });
-}

c19cc8705e75e8b649cbb503f8eabf7498f310b9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-26 21:15:32 -0700

    modify url

--- src/utils/gen-uri.ts
+++ src/utils/gen-uri.ts
@@ -6,10 +6,12 @@ interface Params {
 }
 
 export default function genUri({ username, password, env, port }: Params) {
+  const encoded_username = encodeURIComponent(username).replace(/!/g, "%21");
+  const encoded_password = encodeURIComponent(password).replace(/!/g, "%21");
   if (env === "development") {
-    return `mongodb://${username}:${password}:${port}/myapp`;
+    return `mongodb://127.0.0.1:2701/presidents-api`;
   } else if (env === "production") {
-    return `mongodb+srv://${username}:${password}@cluster0.orpih.mongodb.net/presidents-api?retryWrites=true&w=majority`
+    return `mongodb+srv://${encoded_username}:${encoded_password}@cluster0.orpih.mongodb.net/presidents-api?retryWrites=true&w=majority`
   } else {
     throw "Must specify a node environment in order to generate Mongo URI ... ";
   }

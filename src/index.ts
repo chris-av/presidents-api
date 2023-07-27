6032971cdf33955f30b53bf4826f2ec984bd70c0
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-26 21:15:04 -0700

    refactor to use ApolloServer as middleware to express application

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 require('dotenv').config();
+import http from 'http'
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
 const app = express();
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 import mongoose from 'mongoose';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
-
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import genUri from '@/utils/gen-uri';
+
+
+const httpServer = http.createServer(app);
 
 
 const startServer = async () => {
   const NODE_ENV = process.env.NODE_ENV;
   const MONGO_USERNAME = process.env.MONGO_USERNAME;
   const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+  console.log('detecting the following env variables : ');
+  console.log({ NODE_ENV, MONGO_USERNAME, MONGO_PASSWORD });
+  console.log('');
 
   try {
 
@@ -25,20 +35,29 @@ const startServer = async () => {
       port: 27107,
     });
 
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-    });
-
-
-    // do I stil need both useNewUrlParser and useUnifiedTopology as passed options in new mongoose?
+    console.log('connecting to mongo database at : ' + uri);
     await mongoose.connect(uri);
 
 
-    const { url } = await startStandaloneServer(server, { listen: { port: 8000 } });
 
-    app.listen({ port: port }, () => {
-      console.log(`launching app on port ${url}`);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+    await server.start();
+
+    app.use(
+      cors(),
+      bodyParser.json(),
+      expressMiddleware(server, {}),
+    );
+
+
+
+    httpServer.listen(PORT, () => {
+      console.log(`Server ready at port : ${PORT}`);
     });
 
   } catch (err) {
